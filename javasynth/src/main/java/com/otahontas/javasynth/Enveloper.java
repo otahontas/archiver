e6d5ab32bc248efb0b2979bd0904a9066533de5a
Otto Ahoniemi <43019238+otahontas@users.noreply.github.com>
2019-08-12 09:13:11 +0300

    added first version of cooley-tukey based FFT

---
+++ javasynth/src/main/java/com/otahontas/javasynth/Enveloper.java
@@ -0,0 +1,11 @@
+
+package com.otahontas.javasynth;
+
+/**
+ * This class contains amplitude (Attack, Sustain, Release) envelopers
+ * @author otahontas
+ */
+public class Enveloper {
+    
+    
+}
--- javasynth/src/main/java/com/otahontas/javasynth/Main.java
+++ javasynth/src/main/java/com/otahontas/javasynth/Main.java
@@ -1,17 +1,21 @@
 package com.otahontas.javasynth;
+import java.util.Arrays;
 import java.util.Scanner;
 
 
 public class Main {
     
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Oscillator osc = new Oscillator();
-        SamplePlayer player = new SamplePlayer();
         
-        // Launch text UI
-        Synth synth = new Synth(scanner, osc, player);
-        synth.start();
+        double[] real = {0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1};
+        double[] imag = new double[real.length];
+        
+        FFT fft = new FFT();
+        
+        FFT.transform(real, imag);
+        
+        System.out.println(Arrays.toString(real));
+        System.out.println(Arrays.toString(imag));
         
     }
 
