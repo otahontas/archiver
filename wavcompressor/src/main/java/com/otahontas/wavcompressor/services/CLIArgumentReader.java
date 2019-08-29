87d3f36552ddda557d97825a11f9e1d7d343b5d3
Otto Ahoniemi <43019238+otahontas@users.noreply.github.com>
2019-08-29 10:26:09 +0300

    Finalize CLI argument reader class

--- wavcompressor/src/main/java/com/otahontas/wavcompressor/services/CLIArgumentReader.java
+++ wavcompressor/src/main/java/com/otahontas/wavcompressor/services/CLIArgumentReader.java
@@ -4,53 +4,63 @@ import com.otahontas.wavcompressor.services.CompressorService;
 
 public class CLIArgumentReader {
 
-    private CompressorService cs;
+    private static CompressorService cs = new CompressorService();
     private static String info = "A CLI tool to compress and uncompress wav files";
     private static String usage =
-        "Usage: java jar wavcompressor [-opts] source dest\n"
-        +"e.g. java jar wavcompressor -lc orig.wav comp.wav\n"
+        "Usage: java jar wavcompressor.jar [-opts] source dest\n"
+        +"e.g. java jar wavcompressor.jar -lc orig.wav comp.wav\n"
         +"Opts:\n"
         +"-l Use LZW compression \t -h Use Huffman compression\n"
         +"-c Compress file \t -e Exctract file";
 
-    public CLIArgumentReader(CompressorService cs) {
-        this.cs = cs;
-    }
+    /**
+     * Main reads cli arguments, handles errors and 
+     * calls then compressorservice with user input
+     * */
 
     public static void main(String[] args) {
         if (args.length != 3) {
-            System.out.println(info + "\n" + usage);
-        } else {
-            String flag = args[1];
-            switch (flag) {
-                case "-c":  System.out.println("compress!");
-                            break;
-                case "-e":  System.out.println("extract!");
-                            break;
-                default:    System.out.println("Invalid flag!\n" + usage);
+            printGuide();
+            return;
+        } 
+        
+        String flags = args[0];
+        if (flags.length() != 3 || flags.charAt(0) != '-') {
+            System.out.println("Error: an incorrect usage of flags!\n");
+            printGuide();
+            return;
+        }
+        flags = sortString(flags);
+        String source = args[1];
+        String output = args[2];
+
+        char command = flags.charAt(1);
+        char algo = flags.charAt(2);
+
+        if (!((command == 'e' || command == 'c') && (algo == 'l' || algo == 'h'))) {
+            System.out.println("Error: an incorrect flag!\n");
+            printGuide();
+            return;
+        }
+        cs.run(command, algo, source, output);
+    }
+
+    private static void printGuide() {
+        System.out.println(info + "\n" + usage);
+    }
+
+    /* Since input size is max 3, we can use basic bubble sort */
+    private static String sortString(String str) {
+        char[] token = str.toCharArray();
+        for (int i = 0; i < token.length; i++) {
+            for (int j = i+1; j < token.length; j++) {
+                if (token[i] > token[j]) {
+                    char temp = token[i];
+                    token[i] = token[j];
+                    token[j] = temp;
+                }
             }
         }
+        return new String(token);
     }
-
-/*
-    public void compress(String filename) {
-        FileIO f = new FileIO();
-        byte[] dataBytearray = new byte[0];
-
-        try {
-            dataBytearray = f.readWavFile(filename);
-        } catch (IOException ioe) {
-            System.out.println(ioe);
-        }
-        WavInfoReader wir = new WavInfoReader(dataBytearray);
-        dataBytearray = wir.getDataByteArray();
-        int originalSizeUsed = Byte.SIZE * dataBytearray.length;
-        LZWCompressor lw = new LZWCompressor();
-        lw.addSamples(dataBytearray);
-        byte[] lzwData = lw.compress();
-        int newSizeUsed = Byte.SIZE * lzwData.length;
-        double rate = (double) originalSizeUsed / (double) newSizeUsed;
-        System.out.println("Compression rate: " + rate);
-    }
-*/
 }
--- wavcompressor/src/main/java/com/otahontas/wavcompressor/services/CompressorService.java
+++ wavcompressor/src/main/java/com/otahontas/wavcompressor/services/CompressorService.java
@@ -13,4 +13,58 @@ public class CompressorService {
         this.lzw = new LZWCompressor();
         this.huffman = new HuffmanCompressor();
     }
+
+
+    public void run(char command, char algo, String source, String output) {
+        System.out.println("run the commands");
+    }
 }
+
+
+/*
+        switch (command) {
+            case 'c':   compressorservice.compress()
+                        break;
+            case 'e':   compressorservice.extract()
+                        break;
+            default:    System.out.println("Invalid flag!\n" + usage);
+            }
+        switch (algo) {
+            case 'l':   lzw.compress()
+                        break;
+            case 'h':   System.out.println("extract!");
+                        break;
+            default:    System.out.println("Invalid flag!\n" + usage);
+            }
+        }
+
+    }
+
+    public void extract (char algo) {
+
+    }
+}
+
+
+
+/*
+    public void compress(String filename) {
+        FileIO f = new FileIO();
+        byte[] dataBytearray = new byte[0];
+
+        try {
+            dataBytearray = f.readWavFile(filename);
+        } catch (IOException ioe) {
+            System.out.println(ioe);
+        }
+        WavInfoReader wir = new WavInfoReader(dataBytearray);
+        dataBytearray = wir.getDataByteArray();
+        int originalSizeUsed = Byte.SIZE * dataBytearray.length;
+        LZWCompressor lw = new LZWCompressor();
+        lw.addSamples(dataBytearray);
+        byte[] lzwData = lw.compress();
+        int newSizeUsed = Byte.SIZE * lzwData.length;
+        double rate = (double) originalSizeUsed / (double) newSizeUsed;
+        System.out.println("Compression rate: " + rate);
+    }
+*/
